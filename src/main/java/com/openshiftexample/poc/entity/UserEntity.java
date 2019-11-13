a5abd8d7e00d3819acfaed2b5b0636402811a92c
Anthony Morrison <amorrison@nexient.com>
2019-11-13 15:08:48 -0500

    fixed date time to show created date into db

--- src/main/java/com/openshiftexample/poc/entity/UserEntity.java
+++ src/main/java/com/openshiftexample/poc/entity/UserEntity.java
@@ -1,9 +1,19 @@
 package com.openshiftexample.poc.entity;
 
-import javax.annotation.Generated;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.TemporalType;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Data
@@ -20,7 +30,7 @@ public class UserEntity {
     @Column(name = "email", length = 255)
     private String email;
 
-    @Column(name = "created_date")
-    @Temporal(TemporalType.DATE)
-    private Date date;
+    @Column(name = "created_date", columnDefinition = "DATE")
+    private Date date = new Date();
+    
 }
